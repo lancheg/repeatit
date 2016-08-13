d492e76eb048c8f665a2d2c82479c48255b7d803
lana <xcv505@yandex.ru>
2016-08-13 15:24:03 +0500

    Добавлен генератор карточек

---
+++ src/RepeatIt.Domain/Models/Card.cs
@@ -0,0 +1,38 @@
+﻿using RepeatIt.Domain.Entities;
+
+namespace RepeatIt.Domain.Models
+{
+    public class Card
+    {
+        private Phrase _destinationPhrase;
+        private Phrase _sourcePhrase;
+
+        public Card(Phrase sourcePhrase, Phrase destinationPhrase)
+        {
+            _sourcePhrase = sourcePhrase;
+            _destinationPhrase = destinationPhrase;
+        }
+
+        public string Text
+        {
+            get { return _sourcePhrase.Text; }
+        }
+
+        public string Example
+        {
+            get { return _sourcePhrase.Example; }
+        }
+
+
+        public string TextTranslation
+        {
+            get { return _destinationPhrase.Text; }
+        }
+
+
+        public string ExampleTranslation
+        {
+            get { return _destinationPhrase.Example; }
+        }
+    }
+}
---
+++ src/RepeatIt.Domain/Services/CardGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Core.Domain;
+using RepeatIt.Domain.Entities;
+using RepeatIt.Domain.Models;
+using System.Globalization;
+namespace RepeatIt.Domain.Services
+{
+    public class CardGenerator
+    {
+        private readonly IRepository<Phrase> _phraseRepository;
+
+        public CardGenerator(IRepository<Phrase> phraseRepository)
+        {
+            _phraseRepository = phraseRepository;
+        }
+
+        public Card Generate(CultureInfo sourceCulture, CultureInfo destinationCulture)
+        {
+            var sourcePhrase = _phraseRepository.Get(9);
+            var destinationPhrase = sourcePhrase.GetLocalized(destinationCulture);
+
+            var card = new Card(sourcePhrase, destinationPhrase);
+
+            return card;
+        }
+    }
+}
