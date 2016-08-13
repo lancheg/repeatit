86c43eae3db8fa5946ca485b8cc78f57fa817378
lana <xcv505@yandex.ru>
2016-08-13 15:01:30 +0500

    Подключен EF в проект

--- src/Data/Repository.cs
+++ src/Data/Repository.cs
@@ -1,11 +1,32 @@
-﻿using Core.Domain;
+﻿using System;
+using Core.Domain;
 
 namespace Core.Data.Ef
 {
-    public class Repository<T> where T:IEntity
+    public class Repository<T>: IRepository<IEntity>
     {
         public Repository()
         {
+        }
+
+        public IEntity Add(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ src/Domain/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Domain
+{
+    public interface IRepository<IEntity>
+    {
+        IEntity Add(IEntity entity);
+        IEntity Get(int id);
+        void Delete(int id);
+        void SaveChanges();
+
+    }
+}
